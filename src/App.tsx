949bd3365926c9ba8330a5728ec390876d862437
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2021-05-23 19:17:55 +0000

    added Navbar with a logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-
-
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import {Navbar} from 'react-bootstrap'
+import logo from './bitcoin.svg.png'
 
 const App: React.FC = () => {
-  return <div>
+  return <div className="App">
+    <Navbar bg="dark" variant="dark"
+    sticky="top">
+    <Navbar.Brand>
+      <img src={logo}/> {' '}
+      Logo
+    </Navbar.Brand>
+    </Navbar>
+    <div className="content">
+    This is the content
+    </div>
     </div>
 };
 export default App;
