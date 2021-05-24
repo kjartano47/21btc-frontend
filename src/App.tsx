62c59806fd20e3742fca210a77146e3212839449
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2021-05-24 12:48:49 +0000

    Added dropdown items to Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
 import React from 'react';
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.css';
-import {Navbar} from 'react-bootstrap'
+import {Nav, Navbar, NavDropdown} from 'react-bootstrap'
 import logo from './bitcoin.svg.png'
 
+// interface Props{
+//   title: string;
+//   id: number;
+// }
+
 const App: React.FC = () => {
-  return <div className="App">
+  return (<div className="App">
     <Navbar bg="dark" variant="dark"
-    sticky="top">
+    fixed="top">
     <Navbar.Brand>
-      <img src={logo}/> {' '}
-      Logo
+      <img src={logo} />  {' '}
+    
     </Navbar.Brand>
+    <Nav>
+      <NavDropdown id="123" title="Introduction">
+        <NavDropdown.Item href="introduction/individuals">Introduction</NavDropdown.Item>
+        <NavDropdown.Item href="introduction/businesses">Businesses</NavDropdown.Item>
+        <NavDropdown.Divider/>
+        <NavDropdown.Item href="introduction/businesses">White paper</NavDropdown.Item>
+        </NavDropdown>
+      <Nav.Link href="resources">Resources</Nav.Link>
+      <Nav.Link href="faq">FAQ</Nav.Link>
+    </Nav>
     </Navbar>
     <div className="content">
     This is the content
     </div>
     </div>
+  )
 };
 export default App;
