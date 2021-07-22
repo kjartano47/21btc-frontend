13e825ba642a2d8b856ce54db0b15ac6f0cbb7b6
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2021-07-22 14:35:34 +0000

    fixed buttons

--- src/components/WhitePaperComp.tsx
+++ src/components/WhitePaperComp.tsx
@@ -8,10 +8,10 @@ const WhitePaperComp: React.FC<{}> = () => {
        Choose which translation of the paper you want to read:
 </p>
 <div className="paper-buttons">
-<Paper className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/bitcoin.pdf", "_blank")}>English</Paper>
-<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_de.pdf", "_blank")}>Deutsch</Button>
-<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_fr.pdf", "_blank")}>Français</Button>
-<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_es.pdf", "_blank")}>Español</Button>
+<Button className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/bitcoin.pdf", "_blank")}>English</Button>
+<Button className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_de.pdf", "_blank")}>Deutsch</Button>
+<Button className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_fr.pdf", "_blank")}>Français</Button>
+<Button className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_es.pdf", "_blank")}>Español</Button>
 </div>
 
     </body>
