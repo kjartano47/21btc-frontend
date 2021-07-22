3102760c466335edaabadc1ebf15d643a50736a7
Kjartan Reynir Ólafsson <kjartano17@ru.is>
2021-07-22 14:31:19 +0000

    added more white paper, needs adjustments

--- src/components/WhitePaperComp.tsx
+++ src/components/WhitePaperComp.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from '@material-ui/core';
+import { Button, Paper } from '@material-ui/core';
 
 const WhitePaperComp: React.FC<{}> = () => {
     return (
@@ -7,8 +7,12 @@ const WhitePaperComp: React.FC<{}> = () => {
         <p className="paper"> Satoshi Nakamoto's original paper is still recommended reading for anyone studying how Bitcoin works.
        Choose which translation of the paper you want to read:
 </p>
-<Button variant="contained" onClick={()=> window.open("https://bitcoin.org/bitcoin.pdf", "_blank")}>Original English</Button>
-
+<div className="paper-buttons">
+<Paper className="button-item" variant="outlined" onClick={()=> window.open("https://bitcoin.org/bitcoin.pdf", "_blank")}>English</Paper>
+<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_de.pdf", "_blank")}>Deutsch</Button>
+<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_fr.pdf", "_blank")}>Français</Button>
+<Button className="button-item" variant="contained" onClick={()=> window.open("https://bitcoin.org/files/bitcoin-paper/bitcoin_es.pdf", "_blank")}>Español</Button>
+</div>
 
     </body>
     
